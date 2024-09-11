c9034fd27d7f446f78524bc7266b86b613c9ff19
Kaliandrik <contatokaliandrik@gmail.com>
2024-09-11 15:41:30 -0300

    Verificar número positivo e negativo.

---
+++ atividade13.py
@@ -0,0 +1,9 @@
+# Crie um programa que receba um número do usuário e informe se ele é positivo, negativo ou zero.
+
+numero = float(input("Digite seu número "))
+
+if numero >= 0:
+    print(f" {numero} é positivo.")
+else: 
+    numero > 0
+    print(f" {numero} é negativo. ")
